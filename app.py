988078433f0b0d02dac1ece85225a61892c1a42c
Tonmoy-abc <64578117+Tonmoy-abc@users.noreply.github.com>
2023-11-30 17:42:29 +0600

    updated

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import argparse
 import requests
 import json
+from urllib.parse import urlparse
+
 from bs4 import BeautifulSoup
 from downloader import download
 from error import *
@@ -10,14 +12,14 @@ def saveHtml(name, data):
         f.write(data)
         f.close()
 
-def Download(url, status)->dict:
+def Download(url, status, DEBUG=True)->dict:
     headers = {
         'authority': 'www.tiktok.com',
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'accept-language': 'en-US,en;q=0.9',
         'cache-control': 'no-cache',
         'pragma': 'no-cache',
-        'sec-ch-ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
+        'sec-ch-ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
         'sec-fetch-dest': 'document',
@@ -25,7 +27,7 @@ def Download(url, status)->dict:
         'sec-fetch-site': 'none',
         'sec-fetch-user': '?1',
         'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
     }
 
     session = requests.Session()
@@ -34,12 +36,14 @@ def Download(url, status)->dict:
         raise NotFound(response.status_code)
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    script = soup.select_one('script#SIGI_STATE')
-    jsonData = json.loads(script.contents[0])
+    if DEBUG:
+        saveHtml('res1',soup.prettify())
 
-    author = list(jsonData['UserModule']['users'].keys())[0]
-    video = list(jsonData['ItemModule'].values())[0]['video']
-    createTime =  list(jsonData['ItemModule'].values())[0]['createTime']
+    script = soup.select_one('script#__UNIVERSAL_DATA_FOR_REHYDRATION__')
+    jsonData = json.loads(script.string)["__DEFAULT_SCOPE__"]["webapp.video-detail"]["itemInfo"]["itemStruct"]
+    author = jsonData["author"]["uniqueId"]
+    video = jsonData['video']
+    createTime =  jsonData['createTime']
 
     videoUrl = video['playAddr']
     cover = video['cover']
@@ -55,13 +59,13 @@ def Download(url, status)->dict:
         'pragma': 'no-cache',
         'range': 'bytes=0-',
         'referer': 'https://www.tiktok.com/',
-        'sec-ch-ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
+        'sec-ch-ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
         'sec-fetch-dest': 'video',
         'sec-fetch-mode': 'cors',
         'sec-fetch-site': 'same-site',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
         }
     if status == 1:
         download(videoUrl, './video/'+author+'-'+createTime+'.'+videoFormat, videoFormat, headers, session)
@@ -70,17 +74,22 @@ def Download(url, status)->dict:
 def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(prog='app.py' , description="TikTik Downloader is a python app for downloading TikToks")
     parser.add_argument('-d', '--download', action='store_true', help="Download option")
-    parser.add_argument('--url', '-u', help="URL of the TikTok video")
+    parser.add_argument('--url', '-u', help="URL of the TikTok video",)
     parser.add_argument('--info',  help="For showing video info")
     args = parser.parse_args()
     return args
 
-def main():
-    pass
+def url_validator(url):
+    url_p = urlparse(url)
+    query = url_p.query
+    if url_p.query != "is_from_webapp=1&sender_device=pc":
+        query = "is_from_webapp=1&sender_device=pc"
+    return f"{url_p.scheme}://{url_p.netloc}{url_p.path}?{query}"
 
 if __name__ == '__main__':
     args = parse_arguments()
     if args.url:
+        print(url_validator(args.url))
         if args.download:
             Download(args.url, status=1)
         elif args.info:
--- downloader.py
+++ downloader.py
@@ -1,24 +1,36 @@
-import requests
-from tqdm import tqdm
 import os
+import requests
+from rich.progress import Progress, BarColumn, DownloadColumn, TransferSpeedColumn, TaskID, TimeRemainingColumn
+from error import *
 
 def download(url:str, filePath:str, fileExt,  header:dict, session:requests.Session, chunk_size=8192):
-    if not os.path.exists(os.path.dirname(filePath)):
-        os.makedirs(os.path.dirname(filePath))
+    if not os.path.isdir(os.path.dirname(filePath)):
+        print("Path does not exist! Create the path %s and then try again"%(os.path.dirname(filePath)))
+        exit()
     try:
         session.headers.update(header)
         response = session.get(url, headers=header, stream=True)
         response.raise_for_status()  # Check if the request was successful
         total_size = int(response.headers.get('content-length', 0))
-        with open(filePath, 'wb') as file, tqdm(
-                desc=filePath[:20]+'...'+fileExt,
-                total=total_size,
-                unit='B',
-                unit_scale=True,
-                unit_divisor=1024,
-            ) as progress_bar:
+
+        # Configure Rich Progress
+        progress = Progress(
+            "[progress.description]{task.description}",
+            BarColumn(),
+            DownloadColumn(),
+            TransferSpeedColumn(),
+            TimeRemainingColumn(),
+            transient=True,
+        )
+        task_id = TaskID(filePath[:20]+'...'+fileExt)
+        task = progress.add_task(task_id, total=total_size)
+
+        with open(filePath, 'wb') as file:
+            progress.start()
             for chunk in response.iter_content(chunk_size=chunk_size):
                 file.write(chunk)
-                progress_bar.update(len(chunk))
+                progress.update(task, advance=len(chunk))
+            progress.stop()
+            
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
